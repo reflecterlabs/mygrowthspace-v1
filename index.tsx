4e5ba1987eb4cdb28e347b9e0d28323475131e30
[dyad] <kludge2tmoon@gmail.com>
2026-01-20 22:00:43 -0300

    [dyad] Downgraded React to 18.3.1 to fix Hook conflict error - wrote 4 file(s), added react@18.3.1, react-dom@18.3.1, @types/react@18.3.1, @types/react-dom@18.3.1 package(s)

--- index.tsx
+++ index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './src/index.css';
 import { AuthProvider } from './src/components/AuthProvider';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+const container = document.getElementById('root');
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </React.StrictMode>
+  );
+}
